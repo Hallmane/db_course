f62d9067ce08bc7ff79e7c29a3bb471113848620
Deni Persson <denip@kth.se>
2022-12-04 23:16:08 +0100

    third query

---
+++ task3querylessoninstructor.sql
@@ -0,0 +1,7 @@
+SELECT i.instructor_id, COUNT(l.instructor_id) AS lesson_count
+FROM instructor i
+JOIN lesson l ON l.instructor_id = i.instructor_id
+WHERE l.time >= CURRENT_DATE - INTERVAL '1 MONTH'
+GROUP BY i.instructor_id
+HAVING COUNT(l.instructor_id) > 0
+ORDER BY lesson_count DESC;
