8173e5947d05412cf53a0aa9a09c39040ab356d5
Hallmane <ericssonwilliam96@gmail.com>
2022-12-05 16:52:41 +0100

    added explain analyze script

---
+++ tasks/analyzing_tasks.sql
@@ -0,0 +1,13 @@
+-- task 1
+EXPLAIN ANALYZE SELECT * FROM lesson_type_count_per_month;
+
+-- task 2
+EXPLAIN ANALYZE SELECT * FROM select_zero_sibling;
+EXPLAIN ANALYZE SELECT * FROM select_one_sibling;
+EXPLAIN ANALYZE SELECT * FROM select_two_siblings;
+
+-- task 3
+EXPLAIN ANALYZE SELECT * FROM instructor_pain;
+
+-- task 4
+EXPLAIN ANALYZE SELECT * FROM next_week_ensembles;
