c1d6f1cebb8d3b181f718ef5396895d4335a49ad
Deni Persson <deni.persson@gmail.com>
2023-01-02 18:30:54 +0100

    sibling query

--- tasks/task2.sql
+++ tasks/task2.sql
@@ -1,26 +1,23 @@
-CREATE VIEW select_zero_siblings AS
-SELECT student.* 
-FROM student 
-LEFT JOIN sibling 
-ON student.student_id = sibling.student_id 
-WHERE sibling.sibling_student_id IS NULL;
-
-CREATE VIEW select_one_sibling AS
-SELECT s1.student_id 
-FROM student s1 
-INNER JOIN sibling s2 ON s1.student_id = s2.student_id 
-GROUP BY s1.student_id 
-HAVING COUNT(DISTINCT s2.sibling_student_id) = 1;
-
-CREATE VIEW select_two_siblings AS
-SELECT s1.student_id 
-FROM student s1 
-INNER JOIN sibling s2 ON s1.student_id = s2.student_id 
-GROUP BY s1.student_id 
+CREATE VIEW num_students_by_num_siblings AS
+SELECT 0 as num_siblings, COUNT(*) as num_students
+FROM student
+LEFT JOIN sibling
+ON student.student_id = sibling.student_id
+WHERE sibling.sibling_student_id IS NULL
+UNION
+SELECT 1 as num_siblings, COUNT(*) as num_students
+FROM student s1
+INNER JOIN sibling s2 ON s1.student_id = s2.student_id
+GROUP BY s1.student_id
+HAVING COUNT(DISTINCT s2.sibling_student_id) = 1
+UNION
+SELECT 2 as num_siblings, COUNT(*) as num_students
+FROM student s1
+INNER JOIN sibling s2 ON s1.student_id = s2.student_id
+GROUP BY s1.student_id
 HAVING COUNT(DISTINCT s2.sibling_student_id) = 2;
 
---EXPLAIN ANALYZE
+--To see the results of this query, run: 
+--SELECT * FROM num_students_by_num_siblings;
 
---EXPLAIN ANALYZE SELECT * FROM select_zero_siblings;
---EXPLAIN ANALYZE SELECT * FROM select_one_sibling;
---EXPLAIN ANALYZE SELECT * FROM select_two_siblings;
+--EXPLAIN ANALYZE SELECT * FROM num_students_by_num_siblings;
